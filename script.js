9e3ef975f7bf963559eccfcbc4d8c9633d334dde
Jaydon <pearcejaydon@gmail.com>
2022-07-26 17:04:28 +1000

    Add event listeners and color grid following the user mouse

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const gridContainer = document.querySelector('.grid-container');
 
 createGrid();
 
+//add mouseover listener for every grid element and change the color
+const gridElement = document.querySelectorAll('.grid-element');
+gridElement.forEach(element => element.addEventListener('mouseover', changeColor));
+
+function changeColor(e){
+    this.style.backgroundColor = "black";
+}
+
 function createGrid(){
+    //create grid of certain size (add user input later)
     for(let i = 0; i <= 16; i++){
         for(let j = 0; j <= 16; j++){
-            let gridElement = document.createElement('div');
-            gridElement.classList.add("grid-element");
-            gridContainer.appendChild(gridElement);
+            let gridDiv = document.createElement('div');
+            gridDiv.classList.add("grid-element");
+            gridContainer.appendChild(gridDiv);
         }
     }
 }
